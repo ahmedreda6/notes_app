386464642cf0a46ffcfea00a8c3caa0c1bb4dbd8
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-04-21 05:33:11 +0200

    add crate fetch note method

--- lib/cubits/notes/notes_cubit.dart
+++ lib/cubits/notes/notes_cubit.dart
@@ -1,9 +1,21 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constant.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNoates() async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotsBox);
+      
+      emit(NotesSucess( notesBox.values.toList()));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
