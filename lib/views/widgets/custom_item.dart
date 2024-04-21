791b53263708f11248438f68e62d5c561c25c7cc
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-04-21 06:18:59 +0200

    display data part 2

--- lib/views/widgets/custom_item.dart
+++ lib/views/widgets/custom_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/edit_view_body.dart';
 
 class NotesItem extends StatelessWidget {
-  const NotesItem({super.key});
-
+  const NotesItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -24,16 +25,16 @@ class NotesItem extends StatelessWidget {
           bottom: 24,
         ),
         decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(
+              title: Text(
+                note.title,
+                style: const TextStyle(
                     fontSize: 26,
                     color: Colors.black,
                     fontWeight: FontWeight.bold),
@@ -41,7 +42,7 @@ class NotesItem extends StatelessWidget {
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16, bottom: 16),
                 child: Text(
-                  'Create UR Frist Note With Ahmed Reda',
+                  note.subtitle,
                   style: TextStyle(
                     fontSize: 18,
                     color: Colors.black.withOpacity(0.6),
@@ -59,7 +60,7 @@ class NotesItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'May 21 , 2023',
+                note.data,
                 style: TextStyle(
                   color: Colors.black.withOpacity(0.6),
                   fontSize: 16,
